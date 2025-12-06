f911c7b38ca2aa82c72f2f6567bac63762ec8406
Horux1 <sebastianhc.10@outlook.com>
2025-12-06 18:20:32 +1000

    Initial commit: Astro cat project

---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import { useRef } from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import '../styles/global.css';
+import moon from '../assets/moon.png';
+import land from '../assets/land.png';
+import cat from '../assets/cat.gif';
+
+export default function ParallaxComponent() {
+  const ref = useRef();
+
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            width: '100%',
+            backgroundImage: `url(${moon.src})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land.src})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '100%',
+          }}
+        />
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={cat.src} alt="cat" />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Help me! ↓↓↓</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <h2>Thank you for saving me Liu♥</h2>
+
+          <a href="./src/pages/video.astro" style={{
+            marginTop: '20px',
+            padding: '10px 60px',
+            backgroundColor: 'rgba(19, 19, 19, 0.46)',
+            color: 'white',
+            textDecoration: 'none',
+            borderRadius: '100px',
+            fontWeight: 'bold',
+            fontSize: '30px',
+          }}>
+            Press me!!!
+          </a>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
