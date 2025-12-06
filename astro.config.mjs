27eddf612686b4d5f872829a69266a0f63dd1b35
Horux1 <sebastianhc.10@outlook.com>
2025-12-06 18:52:49 +1000

    cambio en index y github

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  site: 'https://Horux1.github.io',
+  base: '/FinalR', // El nombre de tu repositorio
+  output: 'static',
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+
 
 })
